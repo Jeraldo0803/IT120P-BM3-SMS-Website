4b2a5ad8a75af3cb83c5a6aa2f732d01c87da3f1
Jeraldo0803 <108305413+Jeraldo0803@users.noreply.github.com>
2024-03-07 15:26:08 +0800

    Added super simple text at index

--- index.php
+++ index.php
@@ -5,10 +5,33 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Index Home Page</title>
+
+    <link rel="stylesheet" href="../src/index.css">
 </head>
 
 <body>
+    <header>
+        <div class="header__title">
+            <h2>IT120P-BM3</h2>
+        </div>
+        <div class="headers">
+            <div class="header__tabs">
+                <a href="">Home Page</a>
+            </div>
+            <div class="header__tabs">
+                <a href="">Services</a>
+            </div>
+            <div class="header__tabs">
+                <a href="">About</a>
+            </div>
+        </div>
+    </header>
+
     <h2>Index Home Page</h2>
+
+    <footer>
+
+    </footer>
 </body>
 
 </html>
